9295afe8afcc5ee73c50bc97b1b8f4d758536f2a
Nils Heilemann <nils.heilemann@nimbits.de>
2022-12-07 14:53:37 +0100

    added ssl cert to use https

--- app.py
+++ app.py
@@ -247,4 +247,4 @@ def return_files_calendar():
     return send_file(buf, download_name=filename)
             
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(ssl_context='adhoc', debug=True)
