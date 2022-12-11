091546f276baa07185625f2e746fe12fecdd46c0
Nils Heilemann <nils.heilemann@nimbits.de>
2022-12-11 20:18:22 +0100

    small changes

--- static/js/sidebars.js
+++ static/js/sidebars.js
@@ -1,4 +1,5 @@
 /* gebe body eine Klasse, wenn der Menü-Button gedrückt wird */
 $("#sidebar-toggle").click(function() {
-  $("body").toggleClass( "sidebar-min" );
+  $("body").toggleClass( "sidebarmin" );
+  console.log("Wechsel")
 });
