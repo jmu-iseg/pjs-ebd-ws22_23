403dbb4468b030df341e4ec8cbf6c9832f90344b
Nils Heilemann <nils.heilemann@nimbits.de>
2022-12-11 20:08:32 +0100

    sidebar responsiveness

--- static/js/sidebars.js
+++ static/js/sidebars.js
@@ -1,8 +1,4 @@
-/* global bootstrap: false */
-(() => {
-  'use strict'
-  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  tooltipTriggerList.forEach(tooltipTriggerEl => {
-    new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-})()
+/* gebe body eine Klasse, wenn der Menü-Button gedrückt wird */
+$('[data-bs-toggle="sidebar"]').click(function() {
+  $(body).toggleClass( "sidebar-min" );
+});
