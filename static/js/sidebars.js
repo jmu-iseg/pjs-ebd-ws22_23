6b41fbbfd968325ea3981138300794d03c27534c
Nils Heilemann <nils.heilemann@nimbits.de>
2022-12-12 01:13:41 +0100

    js changes

--- static/js/sidebars.js
+++ static/js/sidebars.js
@@ -3,9 +3,16 @@ $(document).ready(function () {
   $(".sidebar-toggle").on("click", function () {
     $("body").toggleClass("sidebarmin");
     console.log("Wechsel");
+    if(sessionStorage.getItem("key") != "min") {
+      sessionStorage.setItem("sidebar", "min");
+    };
   });
 
   if (sessionStorage.getItem("sidebar") == "min") {
     $("body").addClass("sidebarmin");
   }
+
+  if ($(window).width() < 960) {
+    alert('Less than 960');
+ }
 });
