d006765e9e4d68c671f2512409b22842b32e2329
Nils Heilemann <nils.heilemann@nimbits.de>
2022-12-12 19:28:50 +0100

    added mobile sidebar js

--- static/js/sidebars.js
+++ static/js/sidebars.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
   /* gebe body eine Klasse, wenn der Menü-Button gedrückt wird */
+
+  if ($(window).width() < 960{
+    $(".sidebar-toggle").on("click", function () {
+      $("body").toggleClass("sidebarmax");
+      console.log("Wechsel");
+      if(sessionStorage.getItem("key") != "min") {
+        sessionStorage.setItem("sidebar", "min");
+      } else {
+        sessionStorage.setItem("sidebar", "max");
+      };
+  
+    });
+ } else {
   $(".sidebar-toggle").on("click", function () {
     $("body").toggleClass("sidebarmin");
     console.log("Wechsel");
@@ -10,8 +23,5 @@ $(document).ready(function () {
     };
 
   });
-
-  if ($(window).width() < 960 && sessionStorage.getItem("sidebar") != "min") {
-    $("body").toggleClass("sidebarmin");
  }
 });
