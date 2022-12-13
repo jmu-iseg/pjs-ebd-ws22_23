bc4477fffb45d5da0dca8c5611e3c9092970d891
Philipp6599 <p.arbogast@web.de>
2022-12-13 17:41:43 +0100

    bug fix

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from icalendar import Calendar, Event, vCalAddress, vText
 import io
 import os
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
