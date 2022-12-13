ebf89e86742e9df3bedc55bcae3bc7d4bca42c7f
Nils Heilemann <nils.heilemann@nimbits.de>
2022-12-13 10:18:51 +0100

    Added scss files

--- update_files.sh
+++ update_files.sh
@@ -1,3 +1,6 @@
 cd /var/www/PJS/
 sudo git pull
+sudo sass static/scss/base.scss static/css/base.css
+sudo sass static/scss/optimization.scss static/css/optimization.css
+sudo sass static/scss/sidebar.scss static/css/sidebar.css
 sudo systemctl reload apache2
