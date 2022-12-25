dfbe9f09834e037a53a4154f469f0fd725e428b8
Nils Heilemann <nils.heilemann@nimbits.de>
2022-12-25 15:51:34 +0100

    added opc ua

--- streaming_data_platform/weather_api.py
+++ streaming_data_platform/weather_api.py
@@ -1,27 +1,69 @@
 import requests
 import json
+import logging
+import asyncio
+import sys
+import pandas as pd
+sys.path.insert(0, "..")
 
-# specify the location
-lat = '49.7672'
-long = '9.5183'
+from asyncua import ua, Server
+from asyncua.common.methods import uamethod
 
-# specify the api key
-key = '2976793f8fedfed783bff2512733ed4b'
+@uamethod
+def func(parent, value):
+    return value * 2
 
-# Make the POST request
-response = requests.post('https://pro.openweathermap.org/data/2.5/forecast/climate?units=metric&lat='+lat+'&lon='+long+'&appid='+key)
+async def main():
+    _logger = logging.getLogger('asyncua')
+    # setup the server
+    server = Server()
+    await server.init()
+    server.set_endpoint('opc.tcp://0.0.0.0:4840/opcua/weather/')
 
-# Print the status code of the response
-print(response.status_code) # should return 200
+    # setup the namespace
+    uri = 'http://pjs.uni-wue.de/weather'
+    idx = await server.register_namespace(uri)
 
-# Print the content of the response
-response_content = response.json()
+    # specify the location
+    lat = '49.7672'
+    long = '9.5183'
 
-# Get forecast for every day
-for day in response_content["list"]:
-    print(day)
-    print("____")
+    # specify the api key
+    key = '2976793f8fedfed783bff2512733ed4b'
 
-# safe as json
-#with open('data.json', 'w') as fp:
-#    json.dump(response_content, fp)
+    # Make the POST request
+    response = requests.post('https://pro.openweathermap.org/data/2.5/forecast/climate?units=metric&lat='+lat+'&lon='+long+'&appid='+key)
+
+    # Print the status code of the response
+    print(response.status_code) # should return 200
+
+    # Print the content of the response
+    response_content = response.json()
+
+    # populating our address space
+    # server.nodes, contains links to very common nodes like objects and root
+    obj = await server.nodes.objects.add_object(idx, 'Weather')
+    varDateTime = await obj.add_variable(idx, 'dateTime', 0, ua.VariantType.String)
+    varWeather = await obj.add_variable(idx, 'weather', 0, ua.VariantType.String)
+    
+    # Set Variables to be writable by clients
+    await varDateTime.set_writable()
+    await varWeather.set_writable() 
+    
+    await server.nodes.objects.add_method(ua.NodeId('ServerMethod', 2), ua.QualifiedName('ServerMethod', 2), func, [ua.VariantType.Int64], [ua.VariantType.Int64])
+    _logger.info('Starting server!')
+    async with server:
+        while True:
+            for day in response_content["list"]:
+                valDateTime = 'test'
+                
+                # giving values
+                await varDateTime.write_value(valDateTime)
+                await varWeather.write_value(day)
+                
+                # wait 1 day before repeating
+                #await asyncio.sleep(3600)
+                
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    asyncio.run(main(), debug=True)
