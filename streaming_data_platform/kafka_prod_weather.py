71cabac712b8e717e3f3807eac796276fcf19d81
Nils Heilemann <nils.heilemann@nimbits.de>
2022-12-25 16:11:03 +0100

    changed service to kafka

---
+++ streaming_data_platform/kafka_prod_weather.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from kafka import KafkaProducer
+import requests
+
+# specify the location
+lat = '49.7672'
+long = '9.5183'
+
+# specify the api key
+key = '2976793f8fedfed783bff2512733ed4b'
+
+# Make the POST request
+response = requests.post('https://pro.openweathermap.org/data/2.5/forecast/climate?units=metric&lat='+lat+'&lon='+long+'&appid='+key)
+
+# Print the status code of the response
+print(response.status_code) # should return 200
+
+# Print the content of the response
+response_content = response.json()
+
+# Create a Kafka Producer
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+# Get forecast for every day and push it to kafka
+for day in response_content["list"]:
+    print(day)
+    print("____")
+
+    # Push DateTime as Key and Output (kWh) as Value
+    producer.send('weather', key=bytes(str(dateTimeValue), 'utf-8'),
+              value=bytes(str(outputValue), 'utf-8'))
