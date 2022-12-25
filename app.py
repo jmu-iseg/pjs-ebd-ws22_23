3ff4c9100ccb263eec27554a4593688df711a702
Nils Heilemann <nils.heilemann@nimbits.de>
2022-12-25 17:12:28 +0100

    print users

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(80), nullable=False)
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[
