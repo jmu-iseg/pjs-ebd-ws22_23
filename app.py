e719f3993105e2729452d02eb1c36abac8086a4e
Nils Heilemann <nils.heilemann@nimbits.de>
2023-01-02 14:25:42 +0100

    added accordion

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
+    role = db.Column(db.String(20))
     password = db.Column(db.String(80), nullable=False)
     #role = db.Column(db.String(80), nullable=False)
 
