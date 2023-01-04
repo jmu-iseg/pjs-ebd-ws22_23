69c10e71f2dae661ee175ada7bedd4509b344a34
Nils Heilemann <nils.heilemann@nimbits.de>
2023-01-04 15:10:48 +0100

    Bugfix

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import os
 
+# set template path
+template_dir = os.path.abspath('templates')
+
+# sett app
 app = Flask(__name__)
 from app import main, settings
 
+# run app
 if __name__ == "__main__":
     app.run(ssl_context='adhoc', debug=True)
