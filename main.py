793c825f4d08f033d8d90032ca66ac67617f0a4a
Nils Heilemann <nils.heilemann@nimbits.de>
2023-01-05 15:06:46 +0100

    testing

--- main.py
+++ main.py
@@ -295,6 +295,32 @@ def get_date():
 def submit():
     sendMailForm = SendMailForm()
     if sendMailForm.validate_on_submit():
+        print("_________test_________")
+        a = sendMailForm.mailAddress.data
+        #sender = 'termine@pjs-mail.de'
+        sender = 'termine@pjs-mail.de'
+        receiver = 'nils.heilemann@gmail.com'
+
+        b = sendMailForm.mailText.data
+        msg = MIMEText('This is a test mail')
+        print(a,b)
+
+        msg['Subject'] = 'Test mail'
+        msg['From'] = 'termine@pjs-mail.de'
+        msg['To'] = receiver
+
+        user = 'termine@pjs-mail.de'
+        password = 'jb79G7JLep'
+
+        # Set up connection to the SMTP server
+        with smtplib.SMTP("smtp.ionos.de", 587) as server:
+
+            # Log in to the server
+            server.login(user, password)
+
+            # Send the email
+            server.sendmail(sender, receiver, msg.as_string())
+            print("mail successfully sent")
         return redirect('/')
     return render_template('pages/mail.html', sendMailForm=sendMailForm)    
 # optimization route
