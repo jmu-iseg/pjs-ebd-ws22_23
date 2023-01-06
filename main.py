2a084b40a00c383012cc434b73c140b98426203f
Nils Heilemann <nils.heilemann@nimbits.de>
2023-01-06 12:40:34 +0100

    From Adress changed

--- main.py
+++ main.py
@@ -287,7 +287,7 @@ def get_date():
         msg = MIMEMultipart()
 
         msg['Subject'] = 'Termineinladung'
-        msg['From'] = config['mail']['mail_user']
+        msg['From'] = "Nicolas Neis"
         msg['To'] = receiver
         msgText = MIMEText('<b>%s</b>' % (sendMailForm.mailText.data), 'html')
         msg.attach(msgText)
