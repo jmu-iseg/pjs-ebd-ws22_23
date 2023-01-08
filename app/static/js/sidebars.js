3c8c722a188bc3519e63e17d75976ee185fc106b
Nils Heilemann <nils.heilemann@nimbits.de>
2023-01-08 14:24:44 +0100

    JS Fix

--- app/static/js/sidebars.js
+++ app/static/js/sidebars.js
@@ -2,14 +2,15 @@ $(document).ready(sidebar_switch);
 
 // Klappe das Menü auf und zu, wenn jemand auf den Button .sidebar-toggle klickt
 function sidebar_switch() {
-  // Wenn Mobilansicht --> Aufklappen
-  if ($(window).width() < 992) {
-     // Wechsle Klasse, die das Menü auf- und zuklappt
-    $("body").on("click", function () {
-      // Prüfen, ob noch von anderer Viewport-Size die Klasse zugewiesen ist
-      $( ".sidebar-toggle" ).removeClass( "sidebarmin" );
+  $("body").on("click", function () {
+    // Wenn Mobilansicht --> Aufklappen
+    if ($(window).width() < 992) {
+      // Wechsle Klasse, die das Menü auf- und zuklappt
 
-      $("body").toggleClass("sidebarmax"+$(window).width());
+      // Prüfen, ob noch von anderer Viewport-Size die Klasse zugewiesen ist
+      $(".sidebar-toggle").removeClass("sidebarmin");
+
+      $("body").toggleClass("sidebarmax" + $(window).width());
       console.log("Wechsel Mobil");
       if (sessionStorage.getItem("key") != "min") {
         sessionStorage.setItem("sidebar", "min");
@@ -17,23 +18,20 @@ function sidebar_switch() {
         sessionStorage.setItem("sidebar", "max");
       };
 
-    });
-
-  // Wenn Desktopansicht --> Zuklappen
-  } else {
-    // Wechsle Klasse, die das Menü auf- und zuklappt
-    $(".sidebar-toggle").on("click", function () {
+      // Wenn Desktopansicht --> Zuklappen
+    } else {
+      // Wechsle Klasse, die das Menü auf- und zuklappt
       // Prüfen, ob noch von anderer Viewport-Size die Klasse zugewiesen ist
-      $( "body" ).removeClass( "sidebarmax" );
+      $("body").removeClass("sidebarmax");
 
       $("body").toggleClass("sidebarmin");
-      console.log("Wechsel Desktop"+$(window).width());
+      console.log("Wechsel Desktop" + $(window).width());
       if (sessionStorage.getItem("key") != "min") {
         sessionStorage.setItem("sidebar", "min");
       } else {
         sessionStorage.setItem("sidebar", "max");
       };
 
-    });
-  }
+    };
+  });
 }
