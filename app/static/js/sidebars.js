04acb78f137642961e4629216a292084ec1aa0a6
Nils Heilemann <nils.heilemann@nimbits.de>
2023-01-08 13:32:14 +0100

    Styling

--- app/static/js/sidebars.js
+++ app/static/js/sidebars.js
@@ -1,27 +1,28 @@
-$(document).ready(function () {
-  /* gebe body eine Klasse, wenn der Menü-Button gedrückt wird */
+$(document).ready(sidebar_switch);
+$(window).on('resize', sidebar_switch);
 
-  if ($(window).width() < 960){
+function sidebar_switch() {
+  if ($(window).width() < 992) {
     $(".sidebar-toggle").on("click", function () {
       $("body").toggleClass("sidebarmax");
       console.log("Wechsel");
-      if(sessionStorage.getItem("key") != "min") {
+      if (sessionStorage.getItem("key") != "min") {
         sessionStorage.setItem("sidebar", "min");
       } else {
         sessionStorage.setItem("sidebar", "max");
       };
-  
-    });
- } else {
-  $(".sidebar-toggle").on("click", function () {
-    $("body").toggleClass("sidebarmin");
-    console.log("Wechsel");
-    if(sessionStorage.getItem("key") != "min") {
-      sessionStorage.setItem("sidebar", "min");
-    } else {
-      sessionStorage.setItem("sidebar", "max");
-    };
 
-  });
- }
-});
+    });
+  } else {
+    $(".sidebar-toggle").on("click", function () {
+      $("body").toggleClass("sidebarmin");
+      console.log("Wechsel");
+      if (sessionStorage.getItem("key") != "min") {
+        sessionStorage.setItem("sidebar", "min");
+      } else {
+        sessionStorage.setItem("sidebar", "max");
+      };
+
+    });
+  }
+}
