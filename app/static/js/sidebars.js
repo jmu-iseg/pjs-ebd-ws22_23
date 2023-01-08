782b8fc99157f7ef12fbd4aa32a158f63c4cc41c
Nils Heilemann <nils.heilemann@nimbits.de>
2023-01-08 13:37:15 +0100

    JS Fix

--- app/static/js/sidebars.js
+++ app/static/js/sidebars.js
@@ -1,8 +1,14 @@
 $(document).ready(sidebar_switch);
 $(window).on('resize', sidebar_switch);
 
+// Klappe das Menü auf und zu, wenn jemand auf den Button .sidebar-toggle klickt
 function sidebar_switch() {
+  // Wenn Mobilansicht --> Aufklappen
   if ($(window).width() < 992) {
+    // Prüfen, ob noch von anderer Viewport-Size die Klasse zugewiesen ist
+    $( ".sidebar-toggle" ).removeClass( "sidebarmin" )
+
+     // Wechsle Klasse, die das Menü auf- und zuklappt
     $(".sidebar-toggle").on("click", function () {
       $("body").toggleClass("sidebarmax");
       console.log("Wechsel");
@@ -13,7 +19,13 @@ function sidebar_switch() {
       };
 
     });
+    
+  // Wenn Mobilansicht --> Zuklappen
   } else {
+    // Prüfen, ob noch von anderer Viewport-Size die Klasse zugewiesen ist
+    $( ".sidebar-toggle" ).removeClass( "sidebarmax" )
+
+    // Wechsle Klasse, die das Menü auf- und zuklappt
     $(".sidebar-toggle").on("click", function () {
       $("body").toggleClass("sidebarmin");
       console.log("Wechsel");
