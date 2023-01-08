6020c14e5f820eab0c03f54fc4f97436d2afe1c6
Hannes Metz <hannes.metz@gmx.de>
2023-01-09 00:14:39 +0100

    update files besser machen

--- app/update_files.sh
+++ app/update_files.sh
@@ -1,4 +1,8 @@
 cd /var/www/PJS/
+sudo git add *
+sudo git stash
 sudo git pull
 sudo sass app/static/scss/main.scss app/static/css/main.css
+cd /var/www/
+sudo chmod -R 777 PJS
 sudo systemctl reload apache2
