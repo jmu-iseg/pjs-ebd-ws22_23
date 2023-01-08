3c996c0c8335fbd6acf4ae56bb55817d2b74a13f
Hannes Metz <hannes.metz@gmx.de>
2023-01-08 12:12:28 +0100

    Config auf pymysql angepasst

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = 'thisisasecretkey'
-    SQLALCHEMY_DATABASE_URI = 'mysql://energy:PJS2022@localhost:3306/pjs'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://energy:PJS2022@localhost:3306/pjs'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
