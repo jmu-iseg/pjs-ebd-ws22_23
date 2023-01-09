bb2eb098416dc5c5658c5cf4f4d0faa052cb8f73
Hannes Metz <hannes.metz@gmx.de>
2023-01-09 22:24:53 +0100

    structure

--- app/helper.py
+++ app/helper.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 import requests
 import json
 import os
-from app import app
 
 def flash_errors(form):
     """Flashes form errors"""
@@ -13,29 +12,6 @@ def flash_errors(form):
                 getattr(form, field).label.text,
                 error
             ), 'error')
-
-def get_graph_params():
-    with open(os.path.join(app.root_path, 'graph_settings.json'), 'r') as openfile:
-        params = json.load(openfile)
-
-    if not ('token' in params and 'expiry' in params and datetime.utcnow() < datetime.strptime(params['expiry'], "%m/%d/%Y, %H:%M:%S")):
-        headers = {
-            'Host': 'login.microsoftonline.com',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-        body = {
-            'client_id': params['client'],
-            'scope': 'https://graph.microsoft.com/.default',
-            'client_secret': params['secret'],
-            'grant_type': 'client_credentials'
-        }
-        resp = requests.post(f"https://login.microsoftonline.com/{params['tenant']}/oauth2/v2.0/token", headers=headers, data=body).json()
-        params['token'] = f"Bearer {resp['access_token']}"
-        params['expiry'] = (datetime.utcnow() + timedelta(seconds=(resp['expires_in']) - 120)).strftime("%m/%d/%Y, %H:%M:%S")
-        with open(os.path.join(app.root_path, 'graph_settings.json'), 'w') as outfile:
-            json.dump(params, outfile)
-
-    return params
 
 def get_weekday(day):
     if day == 0:
